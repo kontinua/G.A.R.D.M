b010bee0949f851e67570f42f8a777d6fa03c203
Elifarley C <elifarley@users.noreply.github.com>
2016-06-10 17:40:46 -0300

    Update run.sh

--- ansible_files/run.sh
+++ ansible_files/run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-docker run williamyeh/ansible:ubuntu14.04 \
-  ansible-playbook -i hosts rancher.yml --connection=local --sudo
+docker run \
+--rm --name ansible-runner \
+-v "$PWD"/playbook.yml:/playbook.yml \
+-v "$PWD"/inventory.ini:/etc/ansible/hosts \
+-v "$PWD"/group_vars:/group_vars \
+-v "$PWD"/roles:/roles \
+williamyeh/ansible:ubuntu14.04-onbuild \
+ansible-playbook-wrapper --connection=local --sudo
