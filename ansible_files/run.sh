8c6d81008f5a4707cd32f65a2931ef0617051862
Elifarley C <elifarley@users.noreply.github.com>
2016-06-10 17:41:54 -0300

    Update run.sh

--- ansible_files/run.sh
+++ ansible_files/run.sh
@@ -4,6 +4,7 @@ docker run \
 --rm --name ansible-runner \
 -v "$PWD"/playbook.yml:/playbook.yml \
 -v "$PWD"/inventory.ini:/etc/ansible/hosts \
+-v "$PWD"/hosts:/hosts \
 -v "$PWD"/group_vars:/group_vars \
 -v "$PWD"/roles:/roles \
 williamyeh/ansible:ubuntu14.04-onbuild \
